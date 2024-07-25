57bd0f743be8f3db6c6e832425be27ce6f7ce37c
markuz899 <markuz.89@hotmail.it>
2024-07-25 11:08:45 +0200

    [feat] - new hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,6 +3,8 @@ import useDevice from "./useDevice";
 import useKentico from "./useKentico";
 import usePwa from "./usePwa";
 import useResize from "./useResize";
+import { useCopyToClipboard } from "./useCopyToClipboard";
+import { useIntersectionObserver } from "./useIntersectionObserver";
 import { useDarkMode } from "./useDarkMode";
 
 export {
@@ -11,5 +13,7 @@ export {
   useKentico,
   usePwa,
   useResize,
+  useCopyToClipboard,
+  useIntersectionObserver,
   useDarkMode,
 };
