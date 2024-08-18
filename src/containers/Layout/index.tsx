f89213e42880ab2ce6018477db3997b14e13f2fe
markuz899 <markuz.89@hotmail.it>
2024-08-18 17:04:52 +0200

    minor

--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -74,7 +74,6 @@ const Flex = styled.div`
 const Page = styled.div`
   width: 100%;
   height: 100%;
-  overflow-y: scroll;
 `;
 const Body = styled.div<StyledDivProps>`
   background: ${({ background, theme }) =>
